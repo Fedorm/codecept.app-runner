de794a34926c5921fb8e2015a1fd69679c3e931b
Jakub Płoskonka <ploskonkajakub@gmail.com>
2018-01-08 22:19:26 +0100

    remove appManager.js, keep everything in index.js

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
-const appManager = require('./appManager');
-const { event }  = require('codeceptjs');
+const { exec, spawn }         = require('child_process');
+const { waitUntilUsedOnHost } = require('tcp-port-used');
+const psTree                  = require('ps-tree');
+const { event }               = require('codeceptjs');
+
+function waitForApp(config, callback) {
+  waitUntilUsedOnHost(config.port, config.host, 500, 6000)
+    .then(() => {
+      console.log('Application started, running tests.');
+      callback();
+    }, err => console.log(err));
+}
+
+class AppManager {
+  constructor() {
+    this.app = null;
+  }
+
+  start(config, callback) {
+    console.log(`Starting app at: ${config.host}:${config.port}`);
+
+    this.app = exec(config.appCommand, { cwd: config.appPath }, (err, stdout, stderr) => {
+      stderr && console.log(stderr)
+    });
+    waitForApp(config, callback);
+  }
+
+  close() {
+    this.app.kill();
+    psTree(this.app.pid, (err, children) => {
+      spawn('kill', ['-9'].concat(this.app.pid, children.map(p => p.PID)));
+    })
+  }
+}
+
+const appManager = new AppManager();
 
 module.exports.startApp = function(config, done) {
   event.dispatcher.on(event.all.result, function() {
